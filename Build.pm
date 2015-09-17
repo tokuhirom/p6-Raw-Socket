d73fbea9303b54315a1aa6df0d5b1ddd1f517297
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-09-17 21:12:59 +0900

    remove WINSOCK.
    Panda generate Makefile via Build.pm always. So WINSOCK is not set.

--- Build.pm
+++ Build.pm
@@ -7,6 +7,7 @@ use Shell::Command;
 class Build is Panda::Builder {
     method build($workdir) {
         mkpath "$workdir/blib/lib/Raw/Socket/";
+        %*ENV{'LIBS'} ~= ' -lws2_32' if $*DISTRO.is-win;
         make("$workdir", "$workdir/blib/lib/Raw/Socket/");
     }
 }
--- Configure.pl6
+++ Configure.pl6
@@ -2,5 +2,5 @@ use LibraryMake;
 
 my $destdir = 'lib/Raw/Socket/';
 my %vars = get-vars($destdir);
-%vars{'WINSOCK'} = $*DISTRO.is-win ?? "-lws2_32" !! "";
+%vars{'LIBS'} ~= $*DISTRO.is-win ?? " -lws2_32" !! "";
 process-makefile('.', %vars);
